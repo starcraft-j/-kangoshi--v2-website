f5ea51dab97df38fe480b482306e1945d95c7a70
verandacat <verandacats@yahoo.co.jp>
2021-12-17 10:08:43 +0900

    roo link変更

--- link/roo.php
+++ link/roo.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="1; url=">
+    <meta http-equiv="refresh" content="1; url=https://919.resistance1.com/clk.php?sid=934&iid=31">
     <title>看護roo 公式サイト</title><?php include 'headTag.php'; ?>
   </head>
   <body> 
-    <p></p>
+    <p>公式サイトへ移動中です</p>
   </body>
 </html>
